a7b909a85d14ba6fed51c2568ae5c56085d68477
Supun Chamuditha <3supun@gmail.com>
2024-10-09 10:18:30 +0530

    feat: Add initial project files and structure
    - Add index.css with global font-family styling
    - Add vite.config.js with react plugin configuration
    - Add main.jsx with root component rendering
    - Add .gitignore file
    - Add HomePage component
    - Add README.md with project description
    - Add index.html with basic structure
    - Add Searchbar component
    - Add package.json with project dependencies
    - Add eslint.config.js with eslint rules
    - Add Navbar and NavbarLogged components
    - Add Banner component
    - Add vite.svg logo file

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Banner from '../Components/Banner'
+import JobPortalContent from '../Components/JobPortalContent'
+
+function HomePage() {
+    return (
+        <div>
+            <Banner />
+            <JobPortalContent />
+        </div>
+    )
+}
+
+export default HomePage
